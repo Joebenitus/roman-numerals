9f52dfab67d7d75e514ffc0b16c4ce812a21ea1a
Joseph Karnafel <josephkarnafel@gmail.com>
2020-08-20 12:01:06 -0700

    experiment with regex to filter four of the same letter
    Co-authored-by: Cameron Will <cellardoorsolar@aol.com>
    Co-authored-by: Jaki Widby jaki.widby@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -12,34 +12,38 @@ function toRomanNumeral(number){
   }
   number %= 1000;
   for (let dCount = 0; dCount < parseInt(number / 500); dCount++){
-    arrayNumeral.push("D")
+    arrayNumeral.push("D");
   }
   number %= 500;
   for (let cCount = 0; cCount < parseInt(number / 100); cCount++){
-    arrayNumeral.push("C")
+    arrayNumeral.push("C");
   }
   number %= 100;
   for (let lCount = 0; lCount < parseInt(number / 50); lCount++){
-    arrayNumeral.push("L")
+    arrayNumeral.push("L");
   }
   number %= 50;
   for (let xCount = 0; xCount < parseInt(number / 10); xCount++){
-    arrayNumeral.push("X")
+    arrayNumeral.push("X");
   }
   number %= 10;
   for (let vCount = 0; vCount < parseInt(number / 5); vCount++){
-    arrayNumeral.push("V")
+    arrayNumeral.push("V");
   }
   number %= 5;
   for (let iCount = 0; iCount < parseInt(number); iCount++){
-    arrayNumeral.push("I")
+    arrayNumeral.push("I");
   }
+  
+  
 
+  let newString = arrayNumeral.join("");
 
-  return arrayNumeral;
+  return newString;
 }
 
-
+//if index is more than [0,1,2], slice < index 1, concat next symbol...
+  
 
 // UI Logic
 $(document).ready(function() {
@@ -50,7 +54,13 @@ $(document).ready(function() {
   });
 });
 
-
+// function replaceMultiples(string) {
+//   string.replace(/xxxx/gi "XL");
+//   string.replace(/iiii/gi; "IV");
+//   
+//   string.replace(/)
+//   return string;
+// }
 
 
 
@@ -67,3 +77,18 @@ $(document).ready(function() {
 //       roman = (key[+digits.pop() + (i * 10)] || "") + roman;
 //   return Array(+digits.join("") + 1).join("M") + roman;
 //}
+
+// var result = 0;
+//   var input = romanNumeral.split('');
+
+//   for (var i = 0; i < input.length; i++) {
+//     var currentLetter = DIGIT_VALUES[input[i]];
+//     var nextLetter = DIGIT_VALUES[input[i + 1]];
+//     if (currentLetter === undefined) {
+//       return 'null';
+//     } else {
+//       if (currentLetter < nextLetter) {
+//         result += nextLetter - currentLetter;
+//         i++;
+//       } else {
+//         result += currentLetter;
